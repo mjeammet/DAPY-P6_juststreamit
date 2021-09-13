3e8396390bc8042b758fdb2a7a1b37b7ab6b54e2
mjeammet <marie.jeammet@gmail.com>
2021-09-13 18:16:47 +0200

    adding empty categories

--- static/js/just_stream_it.js
+++ static/js/just_stream_it.js
@@ -1,10 +1,7 @@
-function test(){
-  alert("The javascript file has been correctly loaded.");
-}
 
 const sliders = document.querySelector(".carouselBox");
 // alert(sliders.innerHTML)
-var scrollPerClick = 400;
+var scrollPerClick = 172;
 var ImagePadding = 20;
 
 showMovieData();
@@ -13,50 +10,55 @@ showMovieData();
 var scrollAmount = 0;
 
 function sliderScrollLeft() {
-  // alert(scrollPerClick, " to the left");
-  // sliders.scrollLeft -= 400;
   sliders.scrollTo({
     top: 0, 
     left: (scrollAmount -= scrollPerClick),
     behavior: "smooth"
   });
 
-  // if (scrollAmount <  0) {
-  //   scrollAmount = 0;
-  // }
+  if (scrollAmount <  0) {
+    scrollAmount = 0;
+  }
 }
 
-function sliderScrollRight() {
-  // alert("Going " + scrollPerClick + " to the right.");
-  sliders.scrollTo({
-    top: 0,
-    left: (scrollAmount += scrollPerClick),
-    behavior: "smooth"
-  });
-  // if (scrollAmount <= sliders.scrollWidth - sliders.clientWidth){
-  //   sliders.scrollTo({
-  //     top: 0,
-  //     left: (scrollAmount += scrollPerClick),
-  //     behaviour: "smooth"
-  //   });
-  // }
+function sliderScrollRight() {  
+  if (scrollAmount <= sliders.scrollWidth - sliders.clientWidth){
+    alert(scrollAmount + " ->" + (scrollAmount + scrollPerClick) + " sur " + sliders.scrollWidth);
+    sliders.scrollTo({
+      top: 0,
+      left: (scrollAmount += scrollPerClick),
+      behavior: "smooth"
+    });
+  }
 }
+
 
 function showMovieData(){
   // Loading elements
   // Here insert API elements
-  pics = ["test_images/contact.jpg", "test_images/Enemy.jpg", "img/jsi_logo.png"]
+  ids = ["best_movies", "cat1"];
+  pics = ["test_images/contact.jpg", "test_images/Enemy.jpg", "img/jsi_logo.png"];
+
+  for (id of ids){
+    element = document.querySelector(`#${id}`).querySelector('.carousel').querySelector(".carouselBox")
+    fillElementMovies(element, pics)
+  }  
+}
+
+// takes carousel box element and fills it with appropriate 
+function fillElementMovies(element, pics){  
   let index = 0;
 
   for (pic_index in pics){
-    for (let re = 0; re < 2; re++){
+    for (let re = 0; re < 3; re++){
       index ++;
-      sliders.insertAdjacentHTML(
+      element.insertAdjacentHTML(
         "beforeend",
         `<img class="img-${index}" slider-img" src="./static/${pics[pic_index]}" />`
       )
-    }  
+    }
   }
 
-  scrollPerClick = document.querySelector(".img-1").clientWidth + ImagePadding;
+  // scrollPerClick = document.querySelector(".img-1").offsetWidth + ImagePadding;
+  // alert(scrollPerClick)
 }
