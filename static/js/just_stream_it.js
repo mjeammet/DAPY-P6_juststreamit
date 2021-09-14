f415fe5e942be7814b6b1ac10625f7982c55df65
mjeammet <marie.jeammet@gmail.com>
2021-09-14 21:13:43 +0200

    adding api data for carousels

--- static/js/just_stream_it.js
+++ static/js/just_stream_it.js
@@ -4,9 +4,11 @@ const sliders = document.querySelector(".carouselBox");
 var scrollPerClick = 172;
 var ImagePadding = 20;
 
-showMovieData();
+get_api();
+// console.log(data);
+// showMovieData();
 
-// SCROLLING PART 
+// CAROUSEL-RELATED FUNCTIONS 
 var scrollAmount = 0;
 
 function sliderScrollLeft() {
@@ -23,7 +25,7 @@ function sliderScrollLeft() {
 
 function sliderScrollRight() {  
   if (scrollAmount <= sliders.scrollWidth - sliders.clientWidth){
-    alert(scrollAmount + " ->" + (scrollAmount + scrollPerClick) + " sur " + sliders.scrollWidth);
+    // alert(scrollAmount + " ->" + (scrollAmount + scrollPerClick) + " sur " + sliders.scrollWidth);
     sliders.scrollTo({
       top: 0,
       left: (scrollAmount += scrollPerClick),
@@ -33,32 +35,45 @@ function sliderScrollRight() {
 }
 
 
-function showMovieData(){
-  // Loading elements
-  // Here insert API elements
-  ids = ["best_movies", "cat1"];
-  pics = ["test_images/contact.jpg", "test_images/Enemy.jpg", "img/jsi_logo.png"];
-
-  for (id of ids){
-    element = document.querySelector(`#${id}`).querySelector('.carousel').querySelector(".carouselBox")
-    fillElementMovies(element, pics)
-  }  
-}
+// DATA RELATED PART
 
 // takes carousel box element and fills it with appropriate 
-function fillElementMovies(element, pics){  
+function fillCarousel(element_id, list_of_movies){
+  element = document.querySelector(`#${element_id}`).querySelector('.carousel').querySelector(".carouselBox")
+
   let index = 0;
 
-  for (pic_index in pics){
-    for (let re = 0; re < 3; re++){
+  for (pic_index in list_of_movies){
       index ++;
       element.insertAdjacentHTML(
         "beforeend",
-        `<img class="img-${index}" slider-img" src="./static/${pics[pic_index]}" />`
+        `<img class="img-${index}" slider-img" src="${list_of_movies[pic_index].image_url}" />`
       )
-    }
   }
 
   // scrollPerClick = document.querySelector(".img-1").offsetWidth + ImagePadding;
   // alert(scrollPerClick)
 }
+
+// API
+async function get_api() {
+  ids = ["best_movies", "cat1", "cat2"];
+
+  api_url = "http://localhost:8000/api/v1/titles/" 
+  uri = [api_url + "?sort_by=-imdb_score", 
+        api_url + "?sort_by=-imdb_score&country=Cuba",
+        api_url + "?genre=sci-fi&sort_by=-votes"];
+
+
+
+  for (index in ids){
+    api_url = uri[index];
+    // console.log(uri[index])
+    var test = await fetch(api_url);
+    var data = await test.json();
+    var api_data = data.results;
+  
+    fillCarousel(ids[index], api_data);
+  }
+}
+
