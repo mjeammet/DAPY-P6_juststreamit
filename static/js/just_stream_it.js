0ab5138aef6e06c53b8370c78bdc831a377881b0
mjeammet <marie.jeammet@gmail.com>
2021-09-15 12:42:41 +0200

    adding modal window for each movie tile

--- static/js/just_stream_it.js
+++ static/js/just_stream_it.js
@@ -1,18 +1,19 @@
 
-const sliders = document.querySelector(".carouselBox");
+// const sliders = document.querySelector(".carouselBox");
 // alert(sliders.innerHTML)
 var scrollPerClick = 172;
 var ImagePadding = 20;
 
-get_api();
+get_data();
 // console.log(data);
 // showMovieData();
 
 // CAROUSEL-RELATED FUNCTIONS 
-var scrollAmount = 0;
+var scrollAmount = 0; // make a scrollAmount for each carousle
 
-function sliderScrollLeft() {
-  sliders.scrollTo({
+function sliderScrollLeft(element) {
+  carousel = element.nextElementSibling;
+  carousel.scrollTo({
     top: 0, 
     left: (scrollAmount -= scrollPerClick),
     behavior: "smooth"
@@ -23,10 +24,11 @@ function sliderScrollLeft() {
   }
 }
 
-function sliderScrollRight() {  
-  if (scrollAmount <= sliders.scrollWidth - sliders.clientWidth){
+function sliderScrollRight(element) {
+  carousel = element.previousElementSibling;
+  if (scrollAmount <= carousel.scrollWidth - carousel.clientWidth){
     // alert(scrollAmount + " ->" + (scrollAmount + scrollPerClick) + " sur " + sliders.scrollWidth);
-    sliders.scrollTo({
+    carousel.scrollTo({
       top: 0,
       left: (scrollAmount += scrollPerClick),
       behavior: "smooth"
@@ -34,37 +36,15 @@ function sliderScrollRight() {
   }
 }
 
-
 // DATA RELATED PART
-
 // takes carousel box element and fills it with appropriate 
-function fillCarousel(element_id, list_of_movies){
-  element = document.querySelector(`#${element_id}`).querySelector('.carousel').querySelector(".carouselBox")
-
-  let index = 0;
-
-  for (pic_index in list_of_movies){
-      index ++;
-      element.insertAdjacentHTML(
-        "beforeend",
-        `<img class="img-${index}" slider-img" src="${list_of_movies[pic_index].image_url}" />`
-      )
-  }
-
-  // scrollPerClick = document.querySelector(".img-1").offsetWidth + ImagePadding;
-  // alert(scrollPerClick)
-}
-
-// API
-async function get_api() {
+async function get_data() {
   ids = ["best_movies", "cat1", "cat2"];
 
   api_url = "http://localhost:8000/api/v1/titles/" 
   uri = [api_url + "?sort_by=-imdb_score", 
         api_url + "?sort_by=-imdb_score&country=Cuba",
         api_url + "?genre=sci-fi&sort_by=-votes"];
-
-
 
   for (index in ids){
     api_url = uri[index];
@@ -77,3 +57,66 @@ async function get_api() {
   }
 }
 
+function fillCarousel(element_id, list_of_movies){
+  element = document.getElementById(`${element_id}`).querySelector('.carousel').querySelector(".carouselBox")
+
+  let index = 0;
+
+  for (pic_index in list_of_movies){
+    for (let i = 0; i < 2; i++){
+      index ++;
+      var cover_url = list_of_movies[pic_index].image_url;
+      var movie_id = list_of_movies[pic_index].id;
+      element.insertAdjacentHTML(
+        "beforeend",
+        `<img class="img-${index}" slider-img" src="${cover_url}" onclick="open_details(${movie_id});" />`
+      )
+    }
+  }
+  // scrollPerClick = document.querySelector(".img-1").offsetWidth + ImagePadding;
+  // alert(scrollPerClick)  
+}
+
+var master_container = document.querySelector('.master-container')
+
+function open_details(movie_id) {
+  // Create movie instance
+  var movie = new Movie(movie_id)
+  console.log(movie.id)
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // and fill it with movie infos
+  var modal_content = document.querySelector(`.modal-content`);
+  modal_content.innerHTML = `<p style='color:blue'>${movie.id}</p>`;  
+
+  // Add the <span> element that closes the modal
+  var span = document.createElement('span')
+  span.className = 'close'
+  span.innerHTML = '&times;'
+  modal_content.appendChild(span)
+
+  console.log(modal_content)
+  modal.appendChild(modal_content);
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  } 
+  
+  modal.style.display = "block";
+}
+
+class Movie {
+  constructor(id){
+    this.id = id;
+  }
+}
