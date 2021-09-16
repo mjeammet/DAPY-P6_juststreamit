e78e011333d1801ff5e3c16bcf3e8aaede02910a
mjeammet <marie.jeammet@gmail.com>
2021-09-16 16:34:18 +0200

    better previous and next pages handling

--- static/js/just_stream_it.js
+++ static/js/just_stream_it.js
@@ -1,50 +1,21 @@
-
-// const sliders = document.querySelector(".carouselBox");
-// alert(sliders.innerHTML)
-var scrollPerClick = 172;
-var ImagePadding = 20;
-
 const api_url = "http://localhost:8000/api/v1/titles/";
 const url_dico = {
   'best_movies':  api_url + "?sort_by=-imdb_score",
   'cat1':  api_url + "?sort_by=-imdb_score&country=Cuba",
-  "cat2":  api_url + "?genre=sci-fi&sort_by=-votes"
+  "cat2":  api_url + "?genre=sci-fi&sort_by=-votes",
 };
 // get_best_movie();
 for (carousel_id of Object.keys(url_dico)){
-  fillCarousel(carousel_id, url_dico[carousel_id]);
+  updateCarousel(carousel_id, url_dico[carousel_id]);
 }
 
 // CAROUSEL-RELATED FUNCTIONS 
-function sliderScrollRight(element){
-  var carousel = element.parentNode;  
-  var carousel_id = carousel.id;
-  var url = url_dico[carousel_id]
-
-  update_dico(carousel_id, url, 'next');
-  fillCarousel(carousel_id, url_dico[carousel_id]);
-}
-
-function sliderScrollLeft(element){
-  var carousel = element.parentNode;  
-  var carousel_id = carousel.id;
-  var url = url_dico[carousel_id]
-
-  update_dico(carousel_id, url, 'previous');
-  fillCarousel(carousel_id, url_dico[carousel_id]);
-}
 
 // connecting to the api
 async function get_json_from_api(url) {
   let api_data = await fetch(url)
   let api_json = await api_data.json();
   return api_json;
-}
-
-// DATA RELATED PART
-async function update_dico(carousel_id, url, movement) {
-  let my_json = await get_json_from_api(url);
-  url_dico[carousel_id] = my_json[movement];
 }
 
 // if (category_name == 'best_movies'){
@@ -57,24 +28,38 @@ async function update_dico(carousel_id, url, movement) {
 
 // takes carousel box element and fills it with appropriate movie
 
-async function fillCarousel(element_id, query_url){
+async function updateCarousel(element_id, query_url){
   let json = await get_json_from_api(query_url);
   list_of_movies = json.results
-  element = document.getElementById(`${element_id}`).querySelector(".carouselBox")
-  element.innerHTML = ""
+  carousel = document.getElementById(`${element_id}`)
+  element = carousel.querySelector(".carouselBox")
+  // element.innerHTML = ""
+
+  // update previous and next buttons' onclick behaviours
+  // TODO : disable button if json.next == null
+  let button_previous = carousel.querySelector(".switchLeft");
+  button_previous.onclick = function() { updateCarousel(element_id, json.previous);  }; 
+
+  let button_next = carousel.querySelector(".switchRight");
+  button_next.onclick = function() { updateCarousel(element_id, json.next);  }; 
+
+  // update cover boxes
+  let box = document.createElement("div");
+  box.className = "carouselBox";
 
   let index = 0;
-
   for (pic_index in list_of_movies){
       index ++;
       var cover_url = list_of_movies[pic_index].image_url;
       var movie_id = list_of_movies[pic_index].id;
-      element.insertAdjacentHTML(
+      box.insertAdjacentHTML(
         "beforeend",
         `<img class="img-${index}" slider-img" src="${cover_url}" onclick="open_details(${movie_id});" />`
       )
   }
+  element.replaceChild(box, element.childNodes[1])
 }
+
 
 // MODAL WINDOW-RELATED THINGIES
 var master_container = document.querySelector('.master-container')
